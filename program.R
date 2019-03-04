c8884bd1eea56a389b1ee8d3f10f99d2b32e4588
CowsKing <CowsKing@github.com>
2019-03-04 22:19:52 +0000

    basic function updated

--- program.R
+++ program.R
@@ -3,32 +3,64 @@ pricer <- function(infile, outfile, targetsize){
   colnames(input) <- c("timestamp", 'type', "order-id", "side", "price", "size")
   totalBidSize <- 0
   totalAskSize <- 0
+  income <- 0
+  expense <- 0
   bidBook <- data.frame()
   askBook <- data.frame()
   #for(i in 1:nrow(input)){
-  for(i in 1:3){
+  for(i in 1:6){
     if(input[i,'type'] == 'A'){
       if(input[i, 'side'] == 'B'){
         bidBook <- rbind(bidBook,input[i,c(3,5,6)])
-        print(bidBook)
-        print("bidBook")
+        bidBook <- bidBook[order(-bidBook$price), ]
+        #print(bidBook)
+        #print("bidBook")
       }
       if(input[i, 'side'] == 'S'){
         askBook <- rbind(askBook,input[i,c(3,5,6)])
-        print(askBook)
-        print("askBook")
+        askBook <- askBook[order(askBook$price), ]
+        #print(askBook)
+       
       }
-    #这里少了一个排序方法
     }
     if(input[i, 'type'] == 'R'){
       id <- input[i, 'order-id']
       size <- input[i, 'size']
-      Reduce(id, bidBook, size)
-      Reduce(id, askBook, size)
+      bidBook = Reduce(id, bidBook, size)
+      askBook = Reduce(id, askBook, size)
+    }
+    totalBidSize <- sumSize(bidBook)
+    totalAskSize <- sumSize(askBook)
+
+    
+    if(totalBidSize >= targetsize){
+      calculate <- calculating(bidBook, targetsize)
+      if(calculate!= income){
+        income <- calculate
+        report(input[i, "timestamp"],"S", income)
+      }
+    }
+    else{
+      if(income > 0){
+        report(input[i, "timestamp"],"S", 0)
+        income <- 0
+      }
     }
     
+    if(totalAsksize >= targetsize){
+      calculate <- calculating(askBook, targetsize)
+      if(calculate!= expense){
+        expense <- calculate
+        report(input[i, "timestamp"],"B", expense)
+      }
+    }
+    else{
+      if(expense > 0){
+        report(input[i, "timestamp"],"B", 0)
+        income <- 0
+      }
+    }
   }
-
 
 
 
@@ -37,15 +69,55 @@ pricer <- function(infile, outfile, targetsize){
 
 Reduce <- function(id, book, size){
   for(i in 1:nrow(book)){
-    if (book[i, "order-id"] == id){
-      book[i, "size"] <-  book[i, "size"] - size
-      if(is.na(book[i, "size"])){
-        book <- book[-i,]
+    if(!is.na(book[i, "size"])){
+      if (book[i, "order-id"] == id){
+        book[i, "size"] <-  book[i, "size"] - size
+        if(is.na(book[i, "size"])){
+          book <- book[-i,]
+        }
+        # print(book[i, ])
       }
-      print(book[i, ])
     }
   }
+  return(book)
 }
-sortBook <- function(book) 
-pricer("sample1_in.txt", 1,0)
+
+sumSize <- function(book){
+  if(!is.null(book[1, "order-id"])){
+    #print(book[, "size"])
+    totalSize <- sum(book[, "size"])
+    return(totalSize)
+  }
+  return(0)
+}
+
+calculating <- function(book, targetsize){
+  total <- 0
+  row <- 1
+  while(targetsize > 0 & !is.na(book[row, "size"])){
+    presentSize = book[row, "size"]
+    if(presentSize < targetsize){
+      total <- total + presentSize*book[row, "price"]
+      targetsize <- targetsize - presentSize
+    }
+    else{
+      total <- total + targetsize*book[row, "price"]
+      targetsize <- 0
+    }
+   #print(total)
+    row <- row + 1
+  }
+  return(total)
+}
+
+report <- function(timestamp,side, price){
+  if(price == 0){
+    print(paste(timestamp, side, "NA"))
+  }
+  else{
+    print(paste(timestamp, side, price))
+  }
+}
+
+pricer("sample1_in.txt", 1, 200)
 
