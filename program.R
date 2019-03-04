128cb88a6a851e6eed7ceb635f35a3acd6e41024
CowsKing <CowsKing@github.com>
2019-03-04 17:41:03 +0000

    reduce updated

--- program.R
+++ program.R
@@ -9,26 +9,43 @@ pricer <- function(infile, outfile, targetsize){
   for(i in 1:3){
     if(input[i,'type'] == 'A'){
       if(input[i, 'side'] == 'B'){
-        bidBook <- rbind(bidBook,input[i,c(2,4,5)])
+        bidBook <- rbind(bidBook,input[i,c(3,5,6)])
         print(bidBook)
-        print("bidBook\n")
+        print("bidBook")
       }
       if(input[i, 'side'] == 'S'){
-        askBook <- rbind(askBook,input[i,c(2,4,5)])
+        askBook <- rbind(askBook,input[i,c(3,5,6)])
         print(askBook)
-        print("askBook\n")
+        print("askBook")
       }
     #这里少了一个排序方法
     }
     if(input[i, 'type'] == 'R'){
-      
+      id <- input[i, 'order-id']
+      size <- input[i, 'size']
+      Reduce(id, bidBook, size)
+      Reduce(id, askBook, size)
     }
     
   }
-  
-  
+
+
+
+
 }
 
-pricer("sample1_in.txt", 1,0)
 
+Reduce <- function(id, book, size){
+  for(i in 1:nrow(book)){
+    if (book[i, "order-id"] == id){
+      book[i, "size"] <-  book[i, "size"] - size
+      if(is.na(book[i, "size"])){
+        book <- book[-i,]
+      }
+      print(book[i, ])
+    }
+  }
+}
+sortBook <- function(book) 
+pricer("sample1_in.txt", 1,0)
 
